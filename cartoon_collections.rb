0a904f97a2d33c14e4ab7910928179db41316bdd
Paul Ralyea <047par@gmail.com>
2020-05-12 05:52:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,6 @@ end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
